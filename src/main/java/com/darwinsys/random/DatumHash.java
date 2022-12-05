2ba355b11ffcef59bdec72c5dd88227e4fee64e4
Ian Darwin <ian@darwinsys.com>
2022-12-05 15:58:04 -0500

    Simplify code

--- src/main/java/com/darwinsys/random/DatumHash.java
+++ src/main/java/com/darwinsys/random/DatumHash.java
@@ -1,18 +1,14 @@
 package com.darwinsys.random;
 
-import java.util.*;
-import java.util.stream.IntStream;
-
 public class DatumHash {
 	public static void main(String[] args) {
 		Datum d = new Datum(123, "Ian", 1999);
-		IntStream.range(200, 2020).forEach(i->{
-			if (i % 175 != 0)
-				return;
-			d.setId(i - 2000);
+		// Loop by 175 just to get a good range of values
+		for (int i = 1; i <= 2020; i+=175) {
+			d.setId(2000 % i);
 			d.setYearJoined(i);
 			System.out.println(d +" hashCode: "+d.hashCode());
-		});
+		}
 	}
 }
 
